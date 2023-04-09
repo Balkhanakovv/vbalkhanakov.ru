feb525b431425309056d379cfd9e1d73277d3c24
Balkhanakovv <balkhanakov.vl@yandex.ru>
2023-04-09 10:08:09 +0700

    исправление роутинга

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Main from './Pages/Main';
 import Demos from './Pages/Demos';
 import Apps from './Pages/Apps';
@@ -8,27 +8,18 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />
-  },
-  {
-    path: "/demos",
-    element: <Demos />
-  },
-  {
-    path: "/apps",
-    element: <Apps />
-  },
-  {
-    path: "*",
-    element: <div>404</div>
-  }
-])
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/apps" element={<Apps />} />
+        <Route path="/demos" element={<Demos />} />
+      </Routes>
+    </HashRouter>
+  );
+}
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+  <App/>
 );
